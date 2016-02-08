18a98492b84b8bcded2c806b8c17ffd17b88ad9f
Mattia Migliorini <mattia@squeezyweb.com>
2016-02-08 13:32:34 +0100

    Add RuntimeException Add ExceptionInterface Resolves #1

---
+++ Lib/ExceptionInterface.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Freyja Exception Interface.
+ *
+ * @package Freyja\Exceptions
+ * @copyright 2016 SqueezyWeb
+ * @since 2.1.0
+ */
+
+namespace Freyja\Exceptions;
+
+/**
+ * Interface implemented by all Freyja exceptions.
+ *
+ * @package Freyja\Exceptions
+ * @author Mattia Migliorini <mattia@squeezyweb.com>
+ * @since 2.1.0
+ * @version 1.0.0
+ */
+interface ExceptionInterface {
+  
+}
---
+++ Lib/RuntimeException.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Freyja Runtime Exception.
+ *
+ * @package Freyja\Exceptions
+ * @copyright 2016 SqueezyWeb
+ * @since 2.1.0
+ */
+
+namespace Freyja\Exceptions;
+
+/**
+ * Represent an exception happening at runtime.
+ *
+ * @package Freyja\Excpetions
+ * @author Mattia Migliorini <mattia@squeezyweb.com>
+ * @since 2.1.0
+ * @version 1.0.0
+ */
+class RuntimeException extends \RuntimeException implements ExceptionInterface {
+
+}
